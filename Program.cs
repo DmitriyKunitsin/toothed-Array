0bceb00412fd9021e61ecc1ab9bb3ab2fa38119a
kunicind1996@gmail.com <kunicind1996@gmail.com>
2023-03-01 21:08:05 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ступенчатые__Зубчатые__массивы
+{
+    /*  
+     * Ступенчатые (зубчатые) массивы 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random RandomNumberArray = new Random();
+            int[][] NewArray = new int [3][];
+            NewArray[0] = new int[4];
+            NewArray[1] = new int[6];
+            NewArray[2] = new int[2];
+            for (int i = 0; i < NewArray.Length; i++)
+            {
+                for (int j = 0; j < NewArray[i].Length; j++)
+                {
+                    NewArray[i][j] = RandomNumberArray.Next(100);
+                }
+            }
+            for (int y = 0; y < NewArray.Length; y++)
+            {
+                for (int x = 0; x < NewArray[y].Length; x++)
+                {
+                    Console.Write(NewArray[y][x] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
